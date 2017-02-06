f50ce6c2f3610d1e61b04474a93af15290a8c783
Woye Lin <wl2575@columbia.edu>
2017-02-06 12:14:49 -0500

    fix new dfs

--- asgn1/dfs.py
+++ asgn1/dfs.py
@@ -1,22 +1,49 @@
-import sys, copy, time, resource
+import sys, time, resource
 from math import sqrt
 from collections import deque
+from copy import deepcopy
+
+startTime = time.time()
+
+class Node:
+	# move: the move takes from its parent to itself
+	def __init__(self, move, parent, depth):
+		self.move = move
+		self.parent = parent
+		self.depth = depth
 
 def dfs(initBoard, goalBoard, initIdx, nodes_expanded):
 	frontier, fset, fidx, explored = [initBoard], {initBoard}, [initIdx], set()
+	tree = [Node('', None, 0)]
+
+	max_fringe_size, max_search_depth = 1, 0
 
 	while len(frontier):
 		curBoard = frontier.pop()
 		fset.remove(curBoard)
 		idx = fidx.pop()
 		explored.add(curBoard)
+		node = tree.pop()
 
 		if curBoard == goalBoard:
-			print 'success'
+			path = []
+			while node.parent is not None:
+				path.append(node.move)
+				node = node.parent
+			path.reverse()
+			print 'path_to_goal: ', list(path)
+			print 'cost_of_path: ', len(path)
+			print 'nodes_expanded:', nodes_expanded
+			print 'fringe_size:', len(frontier)
+			print 'max_fringe_size: {}'.format(max_fringe_size)
+			print 'search_depth: {}'.format(node.depth)
+			print 'max_search_depth: {}'.format(max_search_depth)
+			print 'running_time: {}'.format(time.time() - startTime )
+			print 'max_ram_usage: {}'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / float(1024**2))
 			return
 
 		nodes_expanded += 1
-		print nodes_expanded
+		# print nodes_expanded
 
 		delta = int(sqrt(len(curBoard)))
 
@@ -29,6 +56,8 @@ def dfs(initBoard, goalBoard, initIdx, nodes_expanded):
 				frontier.append(neighbor)
 				fset.add(neighbor)
 				fidx.append(idx+1)
+				tree.append(Node('Right', node, node.depth+1))
+				max_search_depth = max(max_search_depth, tree[-1].depth)
 
 		#left
 		if idx % delta:
@@ -39,6 +68,8 @@ def dfs(initBoard, goalBoard, initIdx, nodes_expanded):
 				frontier.append(neighbor)
 				fset.add(neighbor)
 				fidx.append(idx-1)
+				tree.append(Node('Left', node, node.depth+1))
+				max_search_depth = max(max_search_depth, tree[-1].depth)
 
 		#down
 		if idx + delta < len(curBoard):
@@ -49,7 +80,9 @@ def dfs(initBoard, goalBoard, initIdx, nodes_expanded):
 				frontier.append(neighbor)
 				fset.add(neighbor)
 				fidx.append(idx+delta)
-
+				tree.append(Node('Down', node, node.depth+1))
+				max_search_depth = max(max_search_depth, tree[-1].depth)
+				
 		#up
 		if idx - delta >=0:
 			neighbor = list(curBoard)
@@ -59,9 +92,12 @@ def dfs(initBoard, goalBoard, initIdx, nodes_expanded):
 				frontier.append(neighbor)
 				fset.add(neighbor)
 				fidx.append(idx-delta)
+				tree.append(Node('Up', node, node.depth+1))
+				max_search_depth = max(max_search_depth, tree[-1].depth)
+
+		max_fringe_size = max( max_fringe_size, len(frontier))
 
 	return 'Failure'
-
 
 
 if __name__ == '__main__':
