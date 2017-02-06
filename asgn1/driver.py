7fc9247130dfb37fecff3605419b1f76a618ba2d
Woye Lin <wl2575@columbia.edu>
2017-02-05 21:30:55 -0500

    temporary save, may have big changes on represent state

--- asgn1/driver.py
+++ asgn1/driver.py
@@ -6,34 +6,33 @@ nodes_expanded, max_fringe_size, max_search_depth = 0, 0, 0
 start_time = time.time();
 
 class State():
-	def __init__(self, board, path, depth):
+	def __init__(self, board, path, depth, idx):
 		self.board = board
-		self.length = len(board)
-		self.idx = self.board.index(0)
+		self.length = int(sqrt(len(board)))
 		self.path = path
 		self.depth = depth
+		# idx: the index of '0'
+		self.idx = idx
 
 	def up(self):
 		# first check whether the '0' is in the first row
-		checkIdx = self.idx-int(sqrt(self.length))
-		if checkIdx < 0:
+		if self.idx - self.length < 0:
 			return None
 
 		newBoard, newPath = copy.deepcopy(self.board), copy.deepcopy(self.path)
 		newPath.append('Up')
-		newBoard[self.idx], newBoard[checkIdx] = newBoard[checkIdx], newBoard[self.idx]
-		return newBoard, newPath
+		newBoard[self.idx], newBoard[self.idx-self.length] = newBoard[self.idx-self.length], newBoard[self.idx]
+		return tuple(newBoard), newPath, self.idx-self.length
 
 	def down(self):
 		# first check whether the '0' is in the last row
-		checkIdx = self.idx+int(sqrt(self.length))
-		if checkIdx >= self.length:
+		if self.idx + self.length >= len(self.board)
 			return None
 
 		newBoard, newPath = copy.deepcopy(self.board), copy.deepcopy(self.path)
 		newPath.append('Down')
 		newBoard[self.idx], newBoard[checkIdx] = newBoard[checkIdx], newBoard[self.idx]
-		return newBoard, newPath
+		return newBoard, newPath, self.idx+self.length
 
 	def left(self):
 		# first check whether the '0' is in the first column
@@ -43,7 +42,7 @@ class State():
 		newBoard, newPath = copy.deepcopy(self.board), copy.deepcopy(self.path)
 		newPath.append('Left')
 		newBoard[self.idx], newBoard[self.idx-1] = newBoard[self.idx-1], newBoard[self.idx]
-		return newBoard, newPath
+		return newBoard, newPath, int(self.idx % sqrt(self.length))
 
 	def right(self):
 		# first check whether the '0' is in the last column
@@ -53,19 +52,18 @@ class State():
 		newBoard, newPath = copy.deepcopy(self.board), copy.deepcopy(self.path)
 		newPath.append('Right')
 		newBoard[self.idx], newBoard[self.idx+1] = newBoard[self.idx+1], newBoard[self.idx]
-		return newBoard, newPath
+		return newBoard, newPath, int((self.idx+1) % sqrt(self.length))
 
-def notInFrontier(frontier, childBoard):
-	for state in frontier:
-		if state.board == childBoard:
-				return False
-	return True
+# def notInFrontier(frontier, childBoard):
+# 	for state in frontier:
+# 		if state.board == childBoard:
+# 				return False
+# 	return True
 
 def bfs(initBoard, goalBoard):
 	# frontier: deque of State
 	# explored: set of tuple of board
-	frontier = deque([State(initBoard, [], 0)])
-	explored = set()
+	frontier, explored = deque([State(initBoard, [], 0)]), set()
 
 	while len(frontier):
 
@@ -92,7 +90,7 @@ def bfs(initBoard, goalBoard):
 
 		nodes_expanded += 1
 
-		# Up Down Left Right
+		# Up
 		move = curState.up()
 		if move is not None:
 			childBoard, childPath = move
@@ -100,7 +98,7 @@ def bfs(initBoard, goalBoard):
 				# print 'up', childBoard
 				frontier.append(State(childBoard, childPath, curState.depth+1))
 				max_fringe_size, max_search_depth = max(max_fringe_size, len(frontier)), max(max_search_depth, curState.depth+1)
-
+		# Down
 		move = curState.down()
 		if move is not None:
 			childBoard, childPath = move
@@ -109,6 +107,7 @@ def bfs(initBoard, goalBoard):
 				frontier.append(State(childBoard, childPath, curState.depth+1))
 				max_fringe_size, max_search_depth = max(max_fringe_size, len(frontier)),  max(max_search_depth, curState.depth+1)
 
+		# left
 		move = curState.left()
 		if move is not None:
 			childBoard, childPath = move
@@ -117,6 +116,7 @@ def bfs(initBoard, goalBoard):
 				frontier.append(State(childBoard, childPath, curState.depth+1))
 				max_fringe_size,max_search_depth = max(max_fringe_size, len(frontier)),  max(max_search_depth, curState.depth+1)
 
+		# right
 		move = curState.right()
 		if move is not None:
 			childBoard, childPath = move
@@ -127,11 +127,90 @@ def bfs(initBoard, goalBoard):
 
 	return 'Failure'
 
+def dfs(initBoard, goalBoard):
+	# frontier: deque of State
+	# explored: set of tuple of board
+	frontier, frontier_set, explored = [State(initBoard, [], 0, initBoard.index(0))], {tuple(initBoard)}, set()
+	# count = 0
+	# while count < 10:
+	while len(frontier):
+		# count += 1
+		curState = frontier.pop()
+		frontier_set.remove(tuple(curState.board))
+		explored.add(tuple(curState.board))
+
+		global nodes_expanded, max_fringe_size, max_search_depth
+
+		if curState.board == goalBoard:
+			print 'path_to_goal: {}'.format(curState.path)
+			print 'cost_of_path: {}'.format(len(curState.path))
+			print 'nodes_expanded: {}'.format(nodes_expanded)
+			print 'fringe_size: {}'.format(len(frontier))
+			print 'max_fringe_size: {}'.format(max_fringe_size)
+			print 'search_depth: {}'.format(curState.depth)
+			print 'max_search_depth: {}'.format(max_search_depth)
+			print 'running_time: {}'.format(time.time()-start_time)
+			print 'max_ram_usage: {}'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / float(1024**2))
+			return 
+
+		nodes_expanded += 1
+		if nodes_expanded % 100 == 0:
+			print "nodes_expanded = {}".format(nodes_expanded)
+
+		# print 
+		# print 'currentBoard', curState.board
+		# print '====================================='
+
+		# right
+		move = curState.right()
+		if move is not None:
+			childBoard, childPath, idx = move
+			if tuple(childBoard) not in frontier_set  and tuple(childBoard) not in explored:
+				# print 'right', childBoard
+				frontier.append(State(childBoard, childPath, curState.depth+1, idx))
+				frontier_set.add(tuple(childBoard))
+				max_fringe_size,max_search_depth = max(max_fringe_size, len(frontier)),  max(max_search_depth, curState.depth+1)
+
+		# left
+		move = curState.left()
+		if move is not None:
+			childBoard, childPath, idx = move
+			if tuple(childBoard) not in frontier_set and tuple(childBoard) not in explored:
+				# print 'left', childBoard
+				frontier.append(State(childBoard, childPath, curState.depth+1, idx))
+				frontier_set.add(tuple(childBoard))
+				max_fringe_size,max_search_depth = max(max_fringe_size, len(frontier)),  max(max_search_depth, curState.depth+1)
+
+		# down
+		move = curState.down()
+		if move is not None:
+			childBoard, childPath, idx = move
+			if tuple(childBoard) not in frontier_set and tuple(childBoard) not in explored:
+				# print 'down', childBoard
+				frontier.append(State(childBoard, childPath, curState.depth+1, idx))
+				frontier_set.add(tuple(childBoard))
+				max_fringe_size,max_search_depth = max(max_fringe_size, len(frontier)),  max(max_search_depth, curState.depth+1)
+
+		# up
+		move = curState.up()
+		if move is not None:
+			childBoard, childPath, idx = move
+			if tuple(childBoard) not in frontier_set and tuple(childBoard) not in explored:
+				# print 'up', childBoard
+				frontier.append(State(childBoard, childPath, curState.depth+1, idx))
+				frontier_set.add(tuple(childBoard))
+				max_fringe_size,max_search_depth = max(max_fringe_size, len(frontier)),  max(max_search_depth, curState.depth+1)
+
+	return 'Failure'
+
+
 if __name__ == '__main__':
 	start_time = time.time()
 	method, initBoard = sys.argv[1], map(int, sys.argv[2].split(','))
 	if method == 'bfs':
 		bfs(initBoard, range(len(initBoard)))
+	elif method == 'dfs':
+		dfs(initBoard, range(len(initBoard)))
 
 	# fo = open('output.txt', 'wb')
 	# fo.write(str(initBoard))
