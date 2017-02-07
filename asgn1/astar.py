48e2af032312e5d2f3b55a2349db7a4570bbd558
Woye Lin <wl2575@columbia.edu>
2017-02-06 23:14:53 -0500

    fix whole design and move

--- asgn1/astar.py
+++ asgn1/astar.py
@@ -3,6 +3,24 @@ from math import sqrt, fabs
 from collections import deque
 from copy import deepcopy
 
+def h(board):
+	res, length = 0, int(sqrt(len(board)))
+	for curPos, goalPos in enumerate(board):
+		if goalPos:
+			r1, c1, r2, c2 = curPos/length, curPos%length, goalPos/length, goalPos%length
+			res += int(fabs(r1-r2)+ fabs(c1-c2) )
+	return res
+
+def getMove(move):
+	if move == 'Up':
+		return 0
+	elif move == 'Down':
+		return 1
+	elif move == 'Left':
+		return 2
+	else:
+		return 3
+
 class Node:
 	# move: the move takes from its parent to itself
 	def __init__(self, board, idx, move, parent, depth):
@@ -12,64 +30,79 @@ class Node:
 		self.parent = parent
 		self.depth = depth
 
-def up(tree, node, fset, explored, length):
+	def __cmp__(self, other):
+		gself, gother = self.depth + h(self.board), other.depth + h(other.board)
+		if gself < gother or (gself == gother and getMove(self.move) < getMove(other.move) ):
+			return -1
+		return 1
+		
+
+def up(frontier, node, fset, explored, length):
 	if node.idx - length >= 0:
 		newBoard = list(node.board)
 		newBoard[node.idx], newBoard[node.idx-length] = newBoard[node.idx-length], newBoard[node.idx] 
 		newBoard = tuple(newBoard)
 		if newBoard not in fset and newBoard not in explored:
 			child = Node(newBoard, node.idx-length, 'Up', node, node.depth+1 )
-			tree.append(child)
-			fset.add(newBoard)
-			return True
-	return False
+			fset.add(child.board)
+			return child
+		return None
+			# frontier.append(child)
+			# return True
+	# return False
 
 
-def down(tree, node, fset, explored, length):
+def down(frontier, node, fset, explored, length):
 	if node.idx + length < len(node.board):
 		newBoard = list(node.board)
 		newBoard[node.idx], newBoard[node.idx+length] = newBoard[node.idx+length], newBoard[node.idx] 
 		newBoard = tuple(newBoard)
 		if newBoard not in fset and newBoard not in explored:
 			child = Node(newBoard, node.idx+length, 'Down', node, node.depth+1 )
-			tree.append(child)
-			fset.add(newBoard)
-			return True
-	return False
+			fset.add(child.board)
+			return child
+		return None
+	# 		frontier.append(child)
+	# 		return True
+	# return False
 
-def left(tree, node, fset, explored, length):
+def left(frontier, node, fset, explored, length):
 	if node.idx % length:
 		newBoard = list(node.board)
 		newBoard[node.idx], newBoard[node.idx-1] = newBoard[node.idx-1], newBoard[node.idx] 
 		newBoard = tuple(newBoard)
 		if newBoard not in fset and newBoard not in explored:
 			child = Node(newBoard, node.idx-1, 'Left', node, node.depth+1 )
-			tree.append(child)
-			fset.add(newBoard)
-			return True
-	return False
+			fset.add(child.board)
+			return child
+		return None
+	# 		frontier.append(child)
+	# 		return True
+	# return False
 
 
-def right(tree, node, fset, explored, length):
+def right(frontier, node, fset, explored, length):
 	if (node.idx+1) % length:
 		newBoard = list(node.board)
 		newBoard[node.idx], newBoard[node.idx+1] = newBoard[node.idx+1], newBoard[node.idx] 
 		newBoard = tuple(newBoard)
 		if newBoard not in fset and newBoard not in explored:
 			child = Node(newBoard, node.idx+1, 'Right', node, node.depth+1 )
-			tree.append(child)
-			fset.add(newBoard)
-			return True
-	return False
+			fset.add(child.board)
+			return child
+		return None
+	# 		frontier.append(child)
+	# 		return True
+	# return False
 
 
 def dfs(initBoard, goalBoard):
-	max_fringe_size, max_search_depth, nodes_expanded, initIdx = 1, 0, 0, initBoard.index(0)
-	tree, fset, explored = [Node(initBoard, initIdx, '', None, 0)], {initBoard}, set()	
+	max_fringe_size, max_search_depth, nodes_expanded = 1, 0, 0
+	frontier, fset, explored = [Node(initBoard, initBoard.index(0), '', None, 0)], {initBoard}, set()	
 	length = int(sqrt(len(initBoard)))
 	
-	while len(tree):
-		node = tree.pop()
+	while len(frontier):
+		node = frontier.pop()
 		fset.remove(node.board)
 		explored.add(node.board)
 
@@ -83,7 +116,7 @@ def dfs(initBoard, goalBoard):
 			print 'path_to_goal: ', list(path)
 			print 'cost_of_path: ', len(path)
 			print 'nodes_expanded:', nodes_expanded
-			print 'fringe_size:', len(tree)
+			print 'fringe_size:', len(frontier)
 			print 'max_fringe_size: {}'.format(max_fringe_size)
 			print 'search_depth: {}'.format(node.depth)
 			print 'max_search_depth: {}'.format(max_search_depth)
@@ -92,46 +125,68 @@ def dfs(initBoard, goalBoard):
 			return
 
 		nodes_expanded += 1
-		print nodes_expanded
 
-		if right(tree, node, fset, explored, length):
-			max_search_depth = max(max_search_depth, tree[-1].depth)
-		if left(tree, node, fset, explored, length):
-			max_search_depth = max(max_search_depth, tree[-1].depth)
-		if down(tree, node, fset, explored, length):
-			max_search_depth = max(max_search_depth, tree[-1].depth)
-		if up(tree, node, fset, explored, length):
-			max_search_depth = max(max_search_depth, tree[-1].depth)
+		child = right(frontier, node, fset, explored, length)
+		if child is not None:
+			frontier.append(child)
+			max_search_depth = max(max_search_depth, frontier[-1].depth)
+		child = left(frontier, node, fset, explored, length)
+		if child is not None:
+			frontier.append(child)
+			max_search_depth = max(max_search_depth, frontier[-1].depth)
+		child = down(frontier, node, fset, explored, length)
+		if child is not None:
+			frontier.append(child)
+			max_search_depth = max(max_search_depth, frontier[-1].depth)
+		child = up(frontier, node, fset, explored, length)
+		if child is not None:
+			frontier.append(child)
+			max_search_depth = max(max_search_depth, frontier[-1].depth)
 
-		max_fringe_size = max( max_fringe_size, len(tree))
+		max_fringe_size = max( max_fringe_size, len(frontier))
 
-
-def h(board):
-	res, length = 0, int(sqrt(len(board)))
-	for curPos, goalPos in enumerate(board):
-		if goalPos:
-			r1, c1, r2, c2 = curPos/length, curPos%length, goalPos/length, goalPos%length
-			res += int(fabs(r1-r2)+ fabs(c1-c2) )
-	return res
 
 def astar(initBoard, goalBoard):
-	max_fringe_size, max_search_depth, nodes_expanded, initIdx = 1, 0, 0, initBoard.index(0)
-	frontier, fidx, fset, explored = [], [], {initBoard},  set()
+	max_fringe_size, max_search_depth, nodes_expanded = 1, 0, 0
+	frontier, fset, explored = [], {initBoard},  set()
 	heuristic = h(initBoard)
-	heapq.heappush(frontier, (heuristic, initBoard ) )
-	heapq.heappush(fidx, (heuristic, initIdx))
-	tree = []
-	heapq.heappush(tree, (heuristic, Node('', None, 0) ))
+	heapq.heappush(frontier, (heuristic, Node(initBoard, initBoard.index(0), '', None, 0 ) ))
 
 	while len(frontier):
-		curBoard = heapq.heappop(frontier)
-		fset.remove(initBoard)
-		idx = heapq.heappop(fidx)
-		explored.add(curboard)
-		node = heapq.heappop(tree)
+		node = heapq.heappop(frontier)
+		fset.remove(node.board)
+		explored.add(node.board)
 
-		if curBoard == goalBoard:
+		if node.board == goalBoard:
 			print 'SUCCESS'
+
+		nodes_expanded += 1
+
+		child = up(frontier, node, fset, explored, length)
+		if child is not None:
+			heapq.heappush(frontier,child)
+
+
+		child = down(frontier, node, fset, explored, length)
+		if child is not None:
+			heapq.heappush(frontier,child)
+
+
+		child = left(frontier, node, fset, explored, length)
+		if child is not None:
+			heapq.heappush(frontier,child)
+
+
+		child = right(frontier, node, fset, explored, length)
+		if child is not None:
+			heapq.heappush(frontier,child)
+			
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -139,5 +194,4 @@ if __name__ == '__main__':
 	method, initBoard = sys.argv[1], tuple(map(int, sys.argv[2].split(',')))
 	goalBoard = tuple(range(len(initBoard)))
 	dfs(initBoard, goalBoard)
-
 
