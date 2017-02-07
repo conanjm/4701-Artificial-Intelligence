fc4627bde389ef322408a65c5be30b5ad1668841
Woye Lin <wl2575@columbia.edu>
2017-02-07 14:28:04 -0500

    merge bfs

--- asgn1/ast.py
+++ asgn1/ast.py
@@ -103,6 +103,69 @@ def right( node, length):
 	# 		return True
 	# return False
 
+def bfs(initBoard, goalBoard):
+	max_fringe_size, max_search_depth, nodes_expanded, initIdx = 1, 0, 0, initBoard.index(0)
+	frontier, fset, fidx, explored = deque([Node(initBoard, initBoard.index(0), '', None, 0)]), {initBoard}, deque([initIdx]), set()
+
+	while len(frontier):
+		node = frontier.popleft()
+		fset.remove(node.board)
+		explored.add(node.board)
+
+		if node.board == goalBoard:
+			path = []
+			tmp = node
+			while tmp.parent is not None:
+				path.append(tmp.move)
+				tmp = tmp.parent
+			path.reverse()
+			print 'path_to_goal: ', list(path)
+			print 'cost_of_path: ', len(path)
+			print 'nodes_expanded:', nodes_expanded
+			print 'fringe_size:', len(frontier)
+			print 'max_fringe_size: {}'.format(max_fringe_size)
+			print 'search_depth: {}'.format(node.depth)
+			print 'max_search_depth: {}'.format(max_search_depth)
+			print 'running_time: {}'.format(time.time() - startTime )
+			print 'max_ram_usage: {}'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / float(1024**2))
+			return
+
+		nodes_expanded += 1
+		length = int(sqrt(len(node.board)))
+
+		# right
+		newBoard = up( node, length )
+		if newBoard is not None and newBoard not in fset and newBoard not in explored:
+			child = Node(newBoard, node.idx-length, 'Up', node, node.depth+1 )
+			frontier.append(child)
+			fset.add(child.board)
+			max_search_depth = max(max_search_depth, frontier[-1].depth)
+
+		newBoard = down( node, length )
+		if newBoard is not None and newBoard not in fset and newBoard not in explored:
+			child = Node(newBoard, node.idx+length, 'Down', node, node.depth+1 )
+			frontier.append(child)
+			fset.add(child.board)
+			max_search_depth = max(max_search_depth, frontier[-1].depth)
+
+		newBoard = left( node, length )
+		if newBoard is not None and newBoard not in fset and newBoard not in explored:
+			child = Node(newBoard, node.idx-1, 'Left', node, node.depth+1 )
+			frontier.append(child)
+			fset.add(child.board)
+			max_search_depth = max(max_search_depth, frontier[-1].depth)
+
+		newBoard = right( node, length )
+		if newBoard is not None and newBoard not in fset and newBoard not in explored:
+			child = Node(newBoard, node.idx+1, 'Right', node, node.depth+1 )
+			frontier.append(child)
+			fset.add(child.board)
+			max_search_depth = max(max_search_depth, frontier[-1].depth)
+
+		max_fringe_size = max( max_fringe_size, len(frontier))
+
+	return 'Failure'
+
 
 def dfs(initBoard, goalBoard):
 	max_fringe_size, max_search_depth, nodes_expanded = 1, 0, 0
@@ -269,7 +332,9 @@ if __name__ == '__main__':
 	startTime = time.time()
 	method, initBoard = sys.argv[1], tuple(map(int, sys.argv[2].split(',')))
 	goalBoard = tuple(range(len(initBoard)))
-	if method == 'dfs':
+	if method == 'bfs':
+		bfs(initBoard, goalBoard)
+	elif method == 'dfs':
 		dfs(initBoard, goalBoard)
 	elif method == 'ast':
 		ast(initBoard, goalBoard)
