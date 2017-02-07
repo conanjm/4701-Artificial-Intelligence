7e385b80a6d736faddcea9026551c03596ea01b6
Woye Lin <wl2575@columbia.edu>
2017-02-06 20:13:31 -0500

    merge bfs dfs and all moves

--- asgn1/dfs.py
+++ asgn1/dfs.py
@@ -12,11 +12,116 @@ class Node:
 		self.parent = parent
 		self.depth = depth
 
-def dfs(initBoard, goalBoard, initIdx, nodes_expanded):
+def up( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+	if idx - delta >=0:
+		neighbor = list(curBoard)
+		neighbor[idx], neighbor[idx-delta] = neighbor[idx-delta], neighbor[idx]
+		neighbor = tuple(neighbor)
+		if neighbor not in fset and neighbor not in explored:
+			frontier.append(neighbor)
+			fset.add(neighbor)
+			fidx.append(idx-delta)
+			tree.append(Node('Up', node, node.depth+1))
+			return True
+	return False
+
+def down( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+	if idx + delta < len(curBoard):
+		neighbor = list(curBoard)
+		neighbor[idx], neighbor[idx+delta] = neighbor[idx+delta], neighbor[idx]
+		neighbor = tuple(neighbor)
+		if neighbor not in fset and neighbor not in explored:
+			frontier.append(neighbor)
+			fset.add(neighbor)
+			fidx.append(idx+delta)
+			tree.append(Node('Down', node, node.depth+1))
+			return True
+	return False
+
+def left( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+	if idx % delta:
+		neighbor = list(curBoard)
+		neighbor[idx], neighbor[idx-1] =neighbor[idx-1], neighbor[idx]
+		neighbor = tuple(neighbor)
+		if neighbor not in fset and neighbor not in explored:
+			frontier.append(neighbor)
+			fset.add(neighbor)
+			fidx.append(idx-1)
+			tree.append(Node('Left', node, node.depth+1))
+			return True
+	return False
+
+def right( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+	if (idx+1) % delta:
+		neighbor = list(curBoard)
+		neighbor[idx], neighbor[idx+1] = neighbor[idx+1] ,neighbor[idx]
+		neighbor = tuple(neighbor)
+		if neighbor not in fset and neighbor not in explored:
+			frontier.append(neighbor)
+			fset.add(neighbor)
+			fidx.append(idx+1)
+			tree.append(Node('Right', node, node.depth+1))
+			return True
+	return False
+
+def bfs(initBoard, goalBoard):
+	max_fringe_size, max_search_depth, nodes_expanded, initIdx = 1, 0, 0, initBoard.index(0)
+	frontier, fset, fidx, explored = deque([initBoard]), {initBoard}, deque([initIdx]), set()
+	tree = deque([Node('', None, 0)])
+
+	while len(frontier):
+		curBoard = frontier.popleft()
+		fset.remove(curBoard)
+		idx = fidx.popleft()
+		explored.add(curBoard)
+		node = tree.popleft()
+
+		if curBoard == goalBoard:
+			path = []
+			tmp = node
+			while tmp.parent is not None:
+				path.append(tmp.move)
+				tmp = tmp.parent
+			path.reverse()
+			print 'path_to_goal: ', list(path)
+			print 'cost_of_path: ', len(path)
+			print 'nodes_expanded:', nodes_expanded
+			print 'fringe_size:', len(frontier)
+			print 'max_fringe_size: {}'.format(max_fringe_size)
+			print 'search_depth: {}'.format(node.depth)
+			print 'max_search_depth: {}'.format(max_search_depth)
+			print 'running_time: {}'.format(time.time() - startTime )
+			print 'max_ram_usage: {}'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / float(1024**2))
+			return
+
+		nodes_expanded += 1
+		delta = int(sqrt(len(curBoard)))
+
+		# right
+		if up( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+			max_search_depth = max(max_search_depth, tree[-1].depth)	
+
+		if down( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+			max_search_depth = max(max_search_depth, tree[-1].depth)
+
+		if left( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+			max_search_depth = max(max_search_depth, tree[-1].depth)
+
+		if right( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+			max_search_depth = max(max_search_depth, tree[-1].depth)
+
+		max_fringe_size = max( max_fringe_size, len(frontier))
+
+	return 'Failure'
+
+
+
+
+
+def dfs(initBoard, goalBoard):
+	max_fringe_size, max_search_depth, nodes_expanded, initIdx = 1, 0, 0, initBoard.index(0)
 	frontier, fset, fidx, explored = [initBoard], {initBoard}, [initIdx], set()
 	tree = [Node('', None, 0)]
-
-	max_fringe_size, max_search_depth = 1, 0
 
 	while len(frontier):
 		curBoard = frontier.pop()
@@ -49,60 +154,67 @@ def dfs(initBoard, goalBoard, initIdx, nodes_expanded):
 		delta = int(sqrt(len(curBoard)))
 
 		# right
-		if (idx+1) % delta:
-			neighbor = list(curBoard)
-			neighbor[idx], neighbor[idx+1] = neighbor[idx+1] ,neighbor[idx]
-			neighbor = tuple(neighbor)
-			if neighbor not in fset and neighbor not in explored:
-				frontier.append(neighbor)
-				fset.add(neighbor)
-				fidx.append(idx+1)
-				tree.append(Node('Right', node, node.depth+1))
-				max_search_depth = max(max_search_depth, tree[-1].depth)
-				if tree[-1].depth == 0:
-					print tree[-1]
+		if right( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+			max_search_depth = max(max_search_depth, tree[-1].depth)	
 
-		#left
-		if idx % delta:
-			neighbor = list(curBoard)
-			neighbor[idx], neighbor[idx-1] =neighbor[idx-1], neighbor[idx]
-			neighbor = tuple(neighbor)
-			if neighbor not in fset and neighbor not in explored:
-				frontier.append(neighbor)
-				fset.add(neighbor)
-				fidx.append(idx-1)
-				tree.append(Node('Left', node, node.depth+1))
-				max_search_depth = max(max_search_depth, tree[-1].depth)
-				if tree[-1].depth == 0:
-					print tree[-1]
+		if left( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+			max_search_depth = max(max_search_depth, tree[-1].depth)
 
-		#down
-		if idx + delta < len(curBoard):
-			neighbor = list(curBoard)
-			neighbor[idx], neighbor[idx+delta] = neighbor[idx+delta], neighbor[idx]
-			neighbor = tuple(neighbor)
-			if neighbor not in fset and neighbor not in explored:
-				frontier.append(neighbor)
-				fset.add(neighbor)
-				fidx.append(idx+delta)
-				tree.append(Node('Down', node, node.depth+1))
-				max_search_depth = max(max_search_depth, tree[-1].depth)
-				if tree[-1].depth == 0:
-					print tree[-1]
+		if down( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+			max_search_depth = max(max_search_depth, tree[-1].depth)
+
+		if up( curBoard, idx, fset, explored, frontier, fidx, tree, delta, node ):
+			max_search_depth = max(max_search_depth, tree[-1].depth)
+
+
+
+		# if (idx+1) % delta:
+
+		# 	neighbor = list(curBoard)
+		# 	neighbor[idx], neighbor[idx+1] = neighbor[idx+1] ,neighbor[idx]
+		# 	neighbor = tuple(neighbor)
+		# 	if neighbor not in fset and neighbor not in explored:
+		# 		frontier.append(neighbor)
+		# 		fset.add(neighbor)
+		# 		fidx.append(idx+1)
+		# 		tree.append(Node('Right', node, node.depth+1))
+		# 		max_search_depth = max(max_search_depth, tree[-1].depth)
+
+		# #left
+		# if idx % delta:
+		# 	neighbor = list(curBoard)
+		# 	neighbor[idx], neighbor[idx-1] =neighbor[idx-1], neighbor[idx]
+		# 	neighbor = tuple(neighbor)
+		# 	if neighbor not in fset and neighbor not in explored:
+		# 		frontier.append(neighbor)
+		# 		fset.add(neighbor)
+		# 		fidx.append(idx-1)
+		# 		tree.append(Node('Left', node, node.depth+1))
+		# 		max_search_depth = max(max_search_depth, tree[-1].depth)
+
+		# #down
+		# if idx + delta < len(curBoard):
+		# 	neighbor = list(curBoard)
+		# 	neighbor[idx], neighbor[idx+delta] = neighbor[idx+delta], neighbor[idx]
+		# 	neighbor = tuple(neighbor)
+		# 	if neighbor not in fset and neighbor not in explored:
+		# 		frontier.append(neighbor)
+		# 		fset.add(neighbor)
+		# 		fidx.append(idx+delta)
+		# 		tree.append(Node('Down', node, node.depth+1))
+		# 		max_search_depth = max(max_search_depth, tree[-1].depth)
 				
-		#up
-		if idx - delta >=0:
-			neighbor = list(curBoard)
-			neighbor[idx], neighbor[idx-delta] = neighbor[idx-delta], neighbor[idx]
-			neighbor = tuple(neighbor)
-			if neighbor not in fset and neighbor not in explored:
-				frontier.append(neighbor)
-				fset.add(neighbor)
-				fidx.append(idx-delta)
-				tree.append(Node('Up', node, node.depth+1))
-				max_search_depth = max(max_search_depth, tree[-1].depth)
-				if tree[-1].depth == 0:
-					print tree[-1]
+		# #up
+		# if idx - delta >=0:
+		# 	neighbor = list(curBoard)
+		# 	neighbor[idx], neighbor[idx-delta] = neighbor[idx-delta], neighbor[idx]
+		# 	neighbor = tuple(neighbor)
+		# 	if neighbor not in fset and neighbor not in explored:
+		# 		frontier.append(neighbor)
+		# 		fset.add(neighbor)
+		# 		fidx.append(idx-delta)
+		# 		tree.append(Node('Up', node, node.depth+1))
+		# 		max_search_depth = max(max_search_depth, tree[-1].depth)
 
 		max_fringe_size = max( max_fringe_size, len(frontier))
 
@@ -112,4 +224,7 @@ def dfs(initBoard, goalBoard, initIdx, nodes_expanded):
 if __name__ == '__main__':
 	method, initBoard = sys.argv[1], tuple(map(int, sys.argv[2].split(',')))
 	goalBoard = tuple(range(len(initBoard)))
-	dfs(initBoard, goalBoard, initBoard.index(0), 0)
+	if method == 'bfs':
+		bfs(initBoard, goalBoard)
+	elif method == 'dfs':
+		dfs(initBoard, goalBoard)
