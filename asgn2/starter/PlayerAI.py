dc39d9203f781006df7f4034d10e307dc6403c85
Woye Lin <wl2575@columbia.edu>
2017-02-23 21:21:04 -0500

    512

--- asgn2/starter/PlayerAI.py
+++ asgn2/starter/PlayerAI.py
@@ -1,19 +1,23 @@
 from random import *
 from BaseAI import BaseAI
+import time
 
 vecIndex = [UP, DOWN, LEFT, RIGHT] = range(4)
 
 class PlayerAI(BaseAI):
 
+	def __init__(self):
+		self.level = 0
+
 	def numberOfSameTiles(self, grid):
 		count = 0
 		for i in xrange(4):
 			for j in xrange(4):
-				if grid[i][j] == 0:
+				if grid.map[i][j] == 0:
 					continue;
-				if j+1 < 4 and grid[i][j] == grid[i][j+1]:
+				if j+1 < 4 and grid.map[i][j] == grid.map[i][j+1]:
 					count += 1
-				if i+1 < 4 and grid[i][j] == grid[i+1][j]:
+				if i+1 < 4 and grid.map[i][j] == grid.map[i+1][j]:
 					count += 1
 		return float(count) / 25
 
@@ -21,7 +25,7 @@ class PlayerAI(BaseAI):
 		count = 0
 		for i in xrange(4):
 			for j in xrange(4):
-				if grid[i][j] == 0:
+				if grid.map[i][j] == 0:
 					count += 1
 		return float(count) / 16
 
@@ -30,42 +34,39 @@ class PlayerAI(BaseAI):
 		for i in xrange(4):
 			for j in xrange(4):
 				lrIncre, lrDecre, udIncre, udDecre = False, False, False, False
-				if x[i][j] != 0:
-					if j+1 < 4 and x[i][j+1] > x[i][j]:
+				if grid.map[i][j] != 0:
+					if j+1 < 4 and grid.map[i][j+1] > grid.map[i][j]:
 						lrIncre = True
-					if i+1 < 4 and x[i][j] < x[i+1][j]:
+					if i+1 < 4 and grid.map[i][j] < grid.map[i+1][j]:
 						udIncre = True
-				if j+1 < 4 and x[i][j+1] != 0 and x[i][j] > x[i][j+1]:
+				if j+1 < 4 and grid.map[i][j+1] != 0 and grid.map[i][j] > grid.map[i][j+1]:
 					lrDecre = True
-				if i+1 < 4 and x[i+1][j] != 0 and s[i][j] > x[i+1][j]:
-					upDecre = True
+				if i+1 < 4 and grid.map[i+1][j] != 0 and grid.map[i][j] > grid.map[i+1][j]:
+					udDecre = True
 
 			if lrIncre == True and lrDecre == False:
 				lrIncreCnt += 1
-			else if lrIncre == False and lrDecre == True:
+			elif lrIncre == False and lrDecre == True:
 				lrDecreCnt += 1
 			else:
 				pass
-			if upIncre == True and upDecre == False:
-				upIncreCnt += 1
-			else if upIncre == False and upDecre == False:
-				upDecreCnt += 1
+			if udIncre == True and udDecre == False:
+				udIncreCnt += 1
+			elif udIncre == False and udDecre == False:
+				udDecreCnt += 1
 			else:
 				pass
 		return float(max(lrIncreCnt, max(lrDecreCnt, max(udIncreCnt, udDecreCnt)))) / 8
 
-
-	def terminal_test(self, grid):
-
 	def h(self, grid):
+		return 0.1 * self.monotonic(grid) + 0.5 * self.numberOfEmptyTiles(grid) + 0.4 * self.numberOfSameTiles(grid)
 
-	def getChildren(self, moves):
+	def getChildren(self, moves, grid):
 		children = []
 		for move in moves:
 			gridClone = grid.clone()
 			if move == 0:
 				gridClone.moveUD(False)
-				children.append(gridClone)
 			elif move == 1:
 				gridClone.moveUD(True)
 			elif move == 2:
@@ -75,19 +76,24 @@ class PlayerAI(BaseAI):
 			children.append(gridClone)
 		return children
 
+	def terminal_test(self, grid, preTime):
+		if time.clock() - preTime >= 0.0002:
+			preTime = time.clock()
+			return True
+		return False
 
-	def maximize(self, grid, a, b):
+	def maximize(self, grid, a, b, preTime):
 
-		if terminal_test(grid):
-			return None, h(grid)
+		if self.terminal_test(grid, preTime):
+			return None, self.h(grid)
 
 		moves = grid.getAvailableMoves(range(4))
-		children = getChildren(moves)
+		children = self.getChildren(moves, grid)
 
 		maxMove, maxUtility = None, -float("inf")
 
 		for i, child in enumerate(children):
-			_, utility = minimize(child, a, b)
+			_, utility = self.minimize(child, a, b, preTime)
 
 			if utility > maxUtility:
 				maxMove, maxUtility = moves[i], utility
@@ -100,18 +106,18 @@ class PlayerAI(BaseAI):
 		
 		return maxMove, maxUtility
 
-	def minimize(self, grid, a, b):
+	def minimize(self, grid, a, b, preTime):
 
-		if terminal_test(grid):
-			return None, h(grid)
+		if self.terminal_test(grid, preTime):
+			return None, self.h(grid)
 
 		moves = grid.getAvailableMoves(range(4))
-		children = getChildren(moves)
+		children = self.getChildren(moves, grid)
 
 		minMove, minUtility = None, float("inf")
 
 		for i, child in enumerate(children):
-			move, utility = maximize(child, a, b)
+			move, utility = self.maximize(child, a, b, preTime)
 
 			if utility < minUtility:
 				minMove, minUtility = moves[i], utility
@@ -126,8 +132,8 @@ class PlayerAI(BaseAI):
 
 
 	def getMove(self, grid):
-		a, b = -float(inf), float(inf)
-		move, utility = maximize(grid, a, b)
+		a, b = -float("inf"), float("inf")
+		move, utility = self.maximize(grid, a, b, time.clock())
 		return move
 
 
