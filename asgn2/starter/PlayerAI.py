85d7ff31e7e378399d5c47e7427a2c96380e6de7
Woye Lin <wl2575@columbia.edu>
2017-02-24 17:18:09 -0500

    fix monotonic()

--- asgn2/starter/PlayerAI.py
+++ asgn2/starter/PlayerAI.py
@@ -19,10 +19,10 @@ class PlayerAI(BaseAI):
 					count += 1
 				if i+1 < 4 and grid.map[i][j] == grid.map[i+1][j]:
 					count += 1
-		return float(count) / 24
+		return float(count) / 16
 
 	def maxVal(self, grid):
-		res = -float("inf")
+		res = -float("inf") 
 		for i in xrange(4):
 			for j in xrange(4):
 				if res < grid.map[i][j]:
@@ -38,36 +38,51 @@ class PlayerAI(BaseAI):
 		return float(count) / 16
 
 	def monotonic(self, grid):
-		lrIncreCnt, lrDecreCnt, udIncreCnt, udDecreCnt = 0,0,0,0
-		for i in xrange(4):
-			for j in xrange(4):
-				lrIncre, lrDecre, udIncre, udDecre = False, False, False, False
-				if grid.map[i][j] != 0:
-					if j+1 < 4 and grid.map[i][j+1] > grid.map[i][j]:
-						lrIncre = True
-					if i+1 < 4 and grid.map[i][j] < grid.map[i+1][j]:
-						udIncre = True
-				if j+1 < 4 and grid.map[i][j+1] != 0 and grid.map[i][j] > grid.map[i][j+1]:
-					lrDecre = True
-				if i+1 < 4 and grid.map[i+1][j] != 0 and grid.map[i][j] > grid.map[i+1][j]:
-					udDecre = True
+		lrIncre, lrDecre, udIncre, udDecre = 0,0,0,0
 
-			if lrIncre == True and lrDecre == False:
-				lrIncreCnt += 1
-			elif lrIncre == False and lrDecre == True:
-				lrDecreCnt += 1
-			else:
-				pass
-			if udIncre == True and udDecre == False:
-				udIncreCnt += 1
-			elif udIncre == False and udDecre == False:
-				udDecreCnt += 1
-			else:
-				pass
-		return float(max(lrIncreCnt, max(lrDecreCnt, max(udIncreCnt, udDecreCnt)))) / 8
+		for i in xrange(4):
+			if grid.map[i][0] <= grid.map[i][1] and grid.map[i][1] <= grid.map[i][2] and grid.map[i][2] <= grid.map[i][3]:
+				lrIncre += 1
+			if grid.map[i][0] >= grid.map[i][1] and grid.map[i][1] >= grid.map[i][2] and grid.map[i][2] >= grid.map[i][3]:
+				lrDecre += 1
+			if grid.map[0][i] <= grid.map[1][i] and grid.map[1][i] <= grid.map[2][i] and grid.map[2][i] <= grid.map[3][i]:
+				udIncre += 1
+			if grid.map[0][i] >= grid.map[0][i] and grid.map[0][i] >= grid.map[0][i] and grid.map[0][i] >= grid.map[0][i]:
+				udDecre += 1
+
+		return float(max(lrIncre, lrDecre) + max(udIncre, udDecre)) / 8
+
+	# def monotonic(self, grid):
+	# 	lrIncreCnt, lrDecreCnt, udIncreCnt, udDecreCnt = 0,0,0,0
+	# 	for i in xrange(4):
+	# 		for j in xrange(4):
+	# 			lrIncre, lrDecre, udIncre, udDecre = True, True, True, True
+	# 			if grid.map[i][j] != 0:
+	# 				if j+1 < 4 and grid.map[i][j] < grid.map[i][j+1]:
+	# 					lrIncre = lrIncre and True
+	# 				if i+1 < 4 and grid.map[i][j] < grid.map[i+1][j]:
+	# 					udIncre = udIncre and True
+	# 			if j+1 < 4 and grid.map[i][j+1] != 0 and grid.map[i][j] > grid.map[i][j+1]:
+	# 				lrDecre = lrDecre and True
+	# 			if i+1 < 4 and grid.map[i+1][j] != 0 and grid.map[i][j] > grid.map[i+1][j]:
+	# 				udDecre = udDecre and True
+
+	# 		if lrIncre == True and lrDecre == False:
+	# 			lrIncreCnt += 1
+	# 		elif lrIncre == False and lrDecre == True:
+	# 			lrDecreCnt += 1
+	# 		else:
+	# 			pass
+	# 		if udIncre == True and udDecre == False:
+	# 			udIncreCnt += 1
+	# 		elif udIncre == False and udDecre == False:
+	# 			udDecreCnt += 1
+	# 		else:
+	# 			pass
+	# 	return float(max(lrIncreCnt, lrDecreCnt) + max(udIncreCnt, udDecreCnt)) / 8
 
 	def h(self, grid):
-		return  0.7 *self.numberOfEmptyTiles(grid) + 0.3 * self.numberOfSameTiles(grid) 
+		return   0.5 * self.numberOfEmptyTiles(grid) + 0.2 * self.numberOfSameTiles(grid) + 0.3 * self.monotonic(grid)
 
 	def getChildren(self, moves, grid):
 		children = []
@@ -84,15 +99,10 @@ class PlayerAI(BaseAI):
 			children.append(gridClone)
 		return children
 
-	def terminal_test(self, grid, preTime ):
-		if time.clock() - preTime >= 0.0001:
-			preTime = time.clock()
-			return True
-		return False
-
 	def maximize(self, grid, a, b, preTime):
 
-		if self.terminal_test(grid, preTime):
+		if time.clock() - preTime >= 0.0001:
+			preTime = time.clock()
 			return None, self.h(grid)
 
 		moves = grid.getAvailableMoves(range(4))
@@ -116,7 +126,8 @@ class PlayerAI(BaseAI):
 
 	def minimize(self, grid, a, b, preTime):
 
-		if self.terminal_test(grid, preTime):
+		if time.clock() - preTime >= 0.00015:
+			preTime = time.clock()
 			return None, self.h(grid)
 
 		moves = grid.getAvailableMoves(range(4))
