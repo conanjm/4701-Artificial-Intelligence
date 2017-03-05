fb3f705fcbfacc9d99ae9fb5228dd9ec62f3c159
Woye Lin <wl2575@columbia.edu>
2017-03-05 14:00:47 -0500

    0305 1024

--- asgn2/starter/PlayerAI.py
+++ asgn2/starter/PlayerAI.py
@@ -18,45 +18,17 @@ class PlayerAI(BaseAI):
 						denom += 1
 						if grid.map[i][j+1] == grid.map[i][j]:
 							num += 1
+						# if abs(math.log(grid.map[i][j+1],2) if grid.map[i][j+1]!=0 else 0 - math.log(grid.map[i][j], 2) if grid.map[i][j]!=0 else 0) <2:
+						# 	num += 1
 					if i+1<4:
 						denom += 1
 						if grid.map[i+1][j] == grid.map[i][j]:
 							num += 1
+						# if abs(math.log(grid.map[i+1][j], 2) if grid.map[i+1][j]!=0 else 0 - math.log(grid.map[i][j],2) if grid.map[i][j]!=0 else 0) < 2:
+						# 	num += 1
 		if num == 0:
 			return 0
 		return float(num) / denom
-
-	def largeInEdge(self, grid):
-		sum = 0
-		for i in xrange(4):
-			sum += grid.map[i][0]
-			sum += grid.map[i][3]
-		sum += grid.map[0][1] + grid.map[0][2] + grid.map[3][1] + grid.map[3][2]
-		return float(sum) / (8 * grid.getMaxTile())
-
-
-	def maxInCorner(self, grid):
-		return grid.getMaxTile() == grid.map[0][0] or grid.getMaxTile() == grid.map[0][3]  or grid.getMaxTile() == grid.map[3][0] or grid.getMaxTile() == grid.map[3][3] 
-
-	def numberOfSameTiles(self, grid):
-		count = 0
-		for i in xrange(4):
-			for j in xrange(4):
-				if grid.map[i][j] == 0:
-					continue;
-				if j+1 < 4 and grid.map[i][j] == grid.map[i][j+1]:
-					count += math.log(grid.map[i][j], 2)
-				if i+1 < 4 and grid.map[i][j] == grid.map[i+1][j]:
-					count += math.log(grid.map[i][j], 2)
-		return float(count) / (16 * math.log(grid.getMaxTile()))
-
-	def maxVal(self, grid):
-		res = -float("inf") 
-		for i in xrange(4):
-			for j in xrange(4):
-				if res < grid.map[i][j]:
-					res = grid.map[i][j]
-		return math.log(res, 2) / 11
 
 	def numberOfEmptyTiles(self, grid):
 		count = 0
@@ -65,6 +37,18 @@ class PlayerAI(BaseAI):
 				if grid.map[i][j] == 0:
 					count += 1
 		return float(count) / 15
+
+	def snake(self, grid):
+		order = []
+		for i in xrange(4):
+			if grid.map[i][0] >= grid.map[i][1] and grid.map[i][1] >= grid.map[i][2] and grid.map[i][2] >= grid.map[i][3]:
+				order.append(1)
+			elif grid.map[i][0] <= grid.map[i][1] and grid.map[i][1] <= grid.map[i][2] and grid.map[i][2] <= grid.map[i][3]:
+				order.append(-1)
+			else:
+				order.append(0)
+		return order[0]==-order[1] and order[1] == -order[2] and order[2] == -order[3]
+
 
 	def monotonic(self, grid):
 		lrIncre, lrDecre, udIncre, udDecre = 0,0,0,0
@@ -81,38 +65,18 @@ class PlayerAI(BaseAI):
 
 		return float(max(lrIncre, lrDecre) + max(udIncre, udDecre)) / 8
 
-	# def monotonic(self, grid):
-	# 	lrIncreCnt, lrDecreCnt, udIncreCnt, udDecreCnt = 0,0,0,0
-	# 	for i in xrange(4):
-	# 		for j in xrange(4):
-	# 			lrIncre, lrDecre, udIncre, udDecre = True, True, True, True
-	# 			if grid.map[i][j] != 0:
-	# 				if j+1 < 4 and grid.map[i][j] < grid.map[i][j+1]:
-	# 					lrIncre = lrIncre and True
-	# 				if i+1 < 4 and grid.map[i][j] < grid.map[i+1][j]:
-	# 					udIncre = udIncre and True
-	# 			if j+1 < 4 and grid.map[i][j+1] != 0 and grid.map[i][j] > grid.map[i][j+1]:
-	# 				lrDecre = lrDecre and True
-	# 			if i+1 < 4 and grid.map[i+1][j] != 0 and grid.map[i][j] > grid.map[i+1][j]:
-	# 				udDecre = udDecre and True
+	def maxInCorner(self, grid):
+		maxTile = grid.getMaxTile()
+		return (grid.map[0][0]==maxTile) or (grid.map[0][3]==maxTile) or (grid.map[3][0]==maxTile) or (grid.map[3][3]==maxTile) 
 
-	# 		if lrIncre == True and lrDecre == False:
-	# 			lrIncreCnt += 1
-	# 		elif lrIncre == False and lrDecre == True:
-	# 			lrDecreCnt += 1
-	# 		else:
-	# 			pass
-	# 		if udIncre == True and udDecre == False:
-	# 			udIncreCnt += 1
-	# 		elif udIncre == False and udDecre == False:
-	# 			udDecreCnt += 1
-	# 		else:
-	# 			pass
-	#
-	# 	return float(max(lrIncreCnt, lrDecreCnt) + max(udIncreCnt, udDecreCnt)) / 8
+	def maxInUL(self, grid):
+		return grid.getMaxTile() == grid.map[0][0]
 
 	def h(self, grid):
-		return  0.5 * self.numberOfEmptyTiles(grid) + 0.05 * self.monotonic(grid) + 0.45 * self.smooth(grid) 
+		#return  0.46 * self.numberOfEmptyTiles(grid) +  0.22 * self.snake(grid) + 0.27 * self.smooth(grid) + 0.05 * self.maxInCorner(grid)
+		return  0.46 * self.numberOfEmptyTiles(grid) +  0.22 * self.monotonic(grid) + 0.27 * self.smooth(grid) + 0.05 * self.maxInCorner(grid)
+		# return  0.5 * self.numberOfEmptyTiles(grid) + 0.05 * self.monotonic(grid) + 0.45 * self.smooth(grid) 
+
 
 
 	def getChildren(self, moves, grid):
@@ -130,34 +94,9 @@ class PlayerAI(BaseAI):
 			children.append(gridClone)
 		return children
 
-	def maximize(self, grid, a, b, preTime):
+	def minimize(self, grid, a, b, preTime, depth):
 
-		if time.clock() - preTime >= 0.0001:
-			preTime = time.clock()
-			return None, self.h(grid)
-
-		moves = grid.getAvailableMoves(range(4))
-		children = self.getChildren(moves, grid)
-
-		maxMove, maxUtility = None, -float("inf")
-
-		for i, child in enumerate(children):
-			_, utility = self.minimize(child, a, b, preTime)
-
-			if utility > maxUtility:
-				maxMove, maxUtility = moves[i], utility
-
-			if maxUtility >= b:
-				break
-			
-			if maxUtility > a:
-				a = maxUtility
-		
-		return maxMove, maxUtility
-
-	def minimize(self, grid, a, b, preTime):
-
-		if time.clock() - preTime >= 0.0001:
+		if time.clock() - preTime >= 0.00011:
 			preTime = time.clock()
 			return None, self.h(grid)
 
@@ -167,7 +106,7 @@ class PlayerAI(BaseAI):
 		minMove, minUtility = None, float("inf")
 
 		for i, child in enumerate(children):
-			move, utility = self.maximize(child, a, b, preTime)
+			move, utility = self.maximize(child, a, b, preTime, depth+1)
 
 			if utility < minUtility:
 				minMove, minUtility = moves[i], utility
@@ -180,9 +119,33 @@ class PlayerAI(BaseAI):
 		
 		return minMove, minUtility
 
+	def maximize(self, grid, a, b, preTime, depth):
+
+		if time.clock() - preTime >= 0.0001:
+			preTime = time.clock()
+			return None, self.h(grid)
+
+		moves = grid.getAvailableMoves(range(4))
+		children = self.getChildren(moves, grid)
+
+		maxMove, maxUtility = None, -float("inf")
+
+		for i, child in enumerate(children):
+			_, utility = self.minimize(child, a, b, preTime, depth+1)
+
+			if utility > maxUtility:
+				maxMove, maxUtility = moves[i], utility
+
+			if maxUtility >= b:
+				break
+			
+			if maxUtility > a:
+				a = maxUtility
+		
+		return maxMove, maxUtility
 
 	def getMove(self, grid):
 		a, b = -float("inf"), float("inf")
 
-		move, utility = self.maximize(grid, a, b, time.clock())
+		move, utility = self.maximize(grid, a, b, time.clock(), 0)
 		return move
