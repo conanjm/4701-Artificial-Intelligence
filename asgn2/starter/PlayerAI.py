b4d4fd895107bd1b09f9b2695e8c84574b404138
Woye Lin <wl2575@columbia.edu>
2017-03-06 21:10:37 -0500

    achieve 2048

--- asgn2/starter/PlayerAI.py
+++ asgn2/starter/PlayerAI.py
@@ -30,6 +30,19 @@ class PlayerAI(BaseAI):
 			return 0
 		return float(num) / denom
 
+	def smooth2(self, grid):
+		res = 0
+		for i in xrange(4):
+			for j in xrange(4):
+				if grid.map[i][j] != 0:
+					curVal = math.log(grid.map[i][j], 2)
+					if j+1<4 and grid.map[i][j+1] != 0:
+						res -= abs(curVal - math.log(grid.map[i][j+1], 2))
+					if i+1<4 and grid.map[i+1][j] != 0:
+						res -= abs(curVal - math.log(grid.map[i+1][j], 2))
+		return res
+
+
 	def numberOfEmptyTiles(self, grid):
 		count = 0
 		for i in xrange(4):
@@ -37,17 +50,6 @@ class PlayerAI(BaseAI):
 				if grid.map[i][j] == 0:
 					count += 1
 		return float(count) / 15
-
-	def snake(self, grid):
-		order = []
-		for i in xrange(4):
-			if grid.map[i][0] >= grid.map[i][1] and grid.map[i][1] >= grid.map[i][2] and grid.map[i][2] >= grid.map[i][3]:
-				order.append(1)
-			elif grid.map[i][0] <= grid.map[i][1] and grid.map[i][1] <= grid.map[i][2] and grid.map[i][2] <= grid.map[i][3]:
-				order.append(-1)
-			else:
-				order.append(0)
-		return order[0]==-order[1] and order[1] == -order[2] and order[2] == -order[3]
 
 
 	def monotonic(self, grid):
@@ -65,17 +67,67 @@ class PlayerAI(BaseAI):
 
 		return float(max(lrIncre, lrDecre) + max(udIncre, udDecre)) / 8
 
+	def monotonic2(self, grid):
+		totals = [0, 0, 0, 0];
+
+  		# up/down direction
+  		for i in xrange(4):
+  			current = 0
+	    	nxt = current+1
+	    	while nxt < 4:  
+	      		while nxt<4 and grid.map[i][nxt]==0:
+	        		nxt += 1
+	        	if nxt >= 4:
+	        		nxt-= 1
+
+	        	currentValue = 0 if grid.map[i][current]==0 else math.log(grid.map[i][current], 2)
+	        	nxtValue = 0 if grid.map[i][nxt]==0 else math.log(grid.map[i][nxt], 2)
+
+	        	if currentValue > nxtValue:
+	        		totals[0] += nxtValue - currentValue
+	        	elif currentValue < nxtValue:
+	        		totals[1] += currentValue - nxtValue
+	        	current = nxt
+	        	nxt += 1
+	        
+		# left/right direction
+		for i in xrange(4):
+			current = 0
+			nxt = current+1
+			while nxt < 4: 
+				while nxt<4 and grid.map[nxt][i]==0:
+					nxt += 1
+				if nxt>=4:
+					nxt -= 1
+
+				currentValue = 0 if grid.map[current][i]==0 else math.log(grid.map[current][i], 2)
+				nxtValue = 0 if grid.map[nxt][i]==0 else math.log(grid.map[nxt][i], 2)
+
+				if currentValue > nxtValue:
+					totals[2] += nxtValue - currentValue
+				elif currentValue < nxtValue:
+					totals[3] += currentValue - nxtValue
+				current = nxt
+				nxt += 1
+
+		return max(totals[0], totals[1]) + max(totals[2], totals[3]);
+
+
 	def maxInCorner(self, grid):
 		maxTile = grid.getMaxTile()
 		return (grid.map[0][0]==maxTile) or (grid.map[0][3]==maxTile) or (grid.map[3][0]==maxTile) or (grid.map[3][3]==maxTile) 
 
-	def maxInUL(self, grid):
-		return grid.getMaxTile() == grid.map[0][0]
 
 	def h(self, grid):
-		#return  0.46 * self.numberOfEmptyTiles(grid) +  0.22 * self.snake(grid) + 0.27 * self.smooth(grid) + 0.05 * self.maxInCorner(grid)
-		return  0.46 * self.numberOfEmptyTiles(grid) +  0.22 * self.monotonic(grid) + 0.27 * self.smooth(grid) + 0.05 * self.maxInCorner(grid)
+		# return 2.7 * math.log(len(grid.getAvailableCells())) + 1.0 * math.log(grid.getMaxTile(), 2) + 0.1 * self.smooth2(grid) + 1.0 * self.monotonic2(grid)
+		
+		#return  0.46 * self.numberOfEmptyTiles(grid) +  0.22 * self.monotonic(grid) + 0.27 * self.smooth(grid) + 0.05 * self.maxInCorner(grid)
+		#return  0.43 * self.numberOfEmptyTiles(grid) +  0.22 * self.monotonic(grid) + 0.30 * self.smooth(grid) + 0.05 * self.maxInCorner(grid)
+
 		# return  0.5 * self.numberOfEmptyTiles(grid) + 0.05 * self.monotonic(grid) + 0.45 * self.smooth(grid) 
+
+		# this is the one achieved 2048!!!!!
+		return  0.4 * self.numberOfEmptyTiles(grid) +  0.23 * self.monotonic(grid) + 0.32 * self.smooth(grid) + 0.05 * self.maxInCorner(grid)
 
 
 
@@ -96,7 +148,7 @@ class PlayerAI(BaseAI):
 
 	def minimize(self, grid, a, b, preTime, depth):
 
-		if time.clock() - preTime >= 0.00011:
+		if time.clock() - preTime >= 0.0001:
 			preTime = time.clock()
 			return None, self.h(grid)
 
@@ -124,6 +176,7 @@ class PlayerAI(BaseAI):
 		if time.clock() - preTime >= 0.0001:
 			preTime = time.clock()
 			return None, self.h(grid)
+
 
 		moves = grid.getAvailableMoves(range(4))
 		children = self.getChildren(moves, grid)
