b96316b4d27fdf2217738d6cb6f0f5ac0d91dcb5
Woye Lin <wl2575@columbia.edu>
2017-03-24 18:34:13 -0400

    train-test-split done

--- asgn3/assignment3/problem3.py
+++ asgn3/assignment3/problem3.py
@@ -1,5 +1,5 @@
 import numpy as np, scipy, sklearn, csv, sys
-
+from sklearn.model_selection import train_test_split
 
 
 
@@ -11,5 +11,8 @@ if __name__ == '__main__':
 		reader = csv.reader(csvfile, delimiter=',')
 		for row in reader:
 			data.append([float(i) for i in row[:-1]])
-			labels.append(row[-1])
+			labels.append(float(row[-1]))
+
+		data, labels = np.array(data), np.array(labels)	
 		
+		data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.4, stratify=labels)
